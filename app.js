83f4a7d1249dfa04045d12b371ca7544b558a2af
wesley <wez@darknet.co.za>
2016-06-25 05:47:49 +0000

    Add pagination for post listing

--- app.js
+++ app.js
@@ -52,25 +52,43 @@ function listener(req, res) {
 function ProcessRequest (req, res, form, callback) {
     try {
         var purl = url.parse(req.url, true);
-        var postdata = JSON.parse(form);
+        var postdata = JSON.parse(form) || { };
 
         if (purl.pathname == '/api/list') {
+            
             // TODO Cache the post list in memory.
             // Refresh the list every n calls, or if n minutes old.
+            
             fs.readdir('public/posts', function(err, data) {
                 if (err) return callback(err);
-                var postlist = JSON.stringify(namify(data));
+
+                data.sort();
+
+                // calculate pagination values
+                var page = postdata.page || 0;
+                var max = postdata.max || (data.length);
+                var sliceFrom = page * max;
+                var sliceTo = sliceFrom + max;
+
+                console.log('slicing from '+sliceFrom+' to '+sliceTo);
+                
+                var result = { };
+                result.page = page;
+                result.next = (data.length > sliceTo) ? page + 1 : page;
+                result.prev = (page > 0) ? page - 1 : 0;
+                result.posts = namify(data.slice(sliceFrom, sliceTo));
+                var postlist = JSON.stringify(result);
                 return callback(null, postlist);
             });
         }
         
         else if (purl.pathname == '/api/get') {
 
-            if (postdata.name == null) {
-                return callback({'message':'no file name given'});
+            if (postdata.key == null) {
+                return callback({'message':'no post key given'});
             }
             
-            var filename = 'public/posts/' + sanitize(postdata.name);
+            var filename = 'public/posts/' + sanitize(postdata.key);
             
             ReadPost(res, filename, function (err, data) {
                 if (err) {
